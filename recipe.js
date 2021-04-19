02ebc69f3012a767efa2923654ebd29a94f96426
EmirBirlik <ninjakoal2002@gmail.com>
2021-04-19 12:45:45 -0400

    files editted

--- recipe.js
+++ recipe.js
@@ -2,12 +2,14 @@ import data from "./api.js";
 console.log(data);
 var mainsalads = document.getElementById('saladsbutton');
 var maindesserts = document.getElementById('dessertsbutton');
+console.log(maindesserts);
 var mainsoups = document.getElementById("soupsbutton");
+console.log(mainsoups);
+var firstcontentimg = document.getElementById("firstcontentimage");
 mainsalads.addEventListener("click", saladcategoryclick);
-maindesserts.addEventListener("click", dessertcategoryclick);
-mainsoups.addEventListener("click", soupscategoryclick);
+console.log(data['salads'][0].image1);
 function saladcategoryclick() {
-    document.getElementById('firstcontentimage').src = data['salads'][0].image1;
+    document.getElementById('firstcontentimage').src = "https://www.fifteenspatulas.com/wp-content/uploads/2011/10/Caesar-Salad-Fifteen-Spatulas-1-640x959.jpg";
 }
 ;
 function dessertcategoryclick() {
--- recipe.ts
+++ recipe.ts
@@ -2,18 +2,21 @@ import data from "./api.js"
 console.log(data)
 const mainsalads = document.getElementById('saladsbutton');
 const maindesserts= document.getElementById('dessertsbutton');
+console.log(maindesserts)
 const mainsoups = document.getElementById("soupsbutton");
+console.log(mainsoups)
+const firstcontentimg = document.getElementById("firstcontentimage")
 
 mainsalads.addEventListener("click",saladcategoryclick)
-maindesserts.addEventListener("click",dessertcategoryclick)
-mainsoups.addEventListener("click",soupscategoryclick)
 
+
+console.log(data['salads'][0].image1)
 
 
 
 
 function saladcategoryclick(){
-    (document.getElementById('firstcontentimage') as HTMLImageElement).src = data['salads'][0].image1;
+    (document.getElementById('firstcontentimage') as HTMLImageElement).src = "https://www.fifteenspatulas.com/wp-content/uploads/2011/10/Caesar-Salad-Fifteen-Spatulas-1-640x959.jpg";
 };
 
 function dessertcategoryclick(){
