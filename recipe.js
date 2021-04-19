eec8c588efd6b99f1cae4f1a0c92216c8fbbc521
EmirBirlik <ninjakoal2002@gmail.com>
2021-04-19 06:42:24 -0400

    struggling with exports

--- recipe.js
+++ recipe.js
@@ -7,3 +7,17 @@ function api() {
     }
 }
 api();
+var mainsalads = document.getElementById('content');
+;
+var maindesserts = document.getElementById('dessertsbutton');
+var mainsoups = document.getElementById("soupsbutton");
+mainsalads.addEventListener("click", saladcategoryclick);
+maindesserts.addEventListener("click", dessertcategoryclick);
+mainsoups.addEventListener("click", soupscategoryclick);
+function saladcategoryclick() {
+    document.getElementById('firstcontentimage').src = data['salads'][0].image1;
+}
+function dessertcategoryclick() {
+}
+function soupscategoryclick() {
+}
--- recipe.ts
+++ recipe.ts
@@ -8,13 +8,27 @@ api()
 
 
 
+const mainsalads: HTMLElement = document.getElementById('content');;
+const maindesserts = document.getElementById('dessertsbutton');
+const mainsoups = document.getElementById("soupsbutton");
+
+mainsalads.addEventListener("click",saladcategoryclick)
+maindesserts.addEventListener("click",dessertcategoryclick)
+mainsoups.addEventListener("click",soupscategoryclick)
 
 
 
 
 
+function saladcategoryclick(){
+    (document.getElementById('firstcontentimage') as HTMLImageElement).src = data['salads'][0].image1;
+}
 
+function dessertcategoryclick(){
 
+}
 
+function soupscategoryclick(){
 
+}
 
