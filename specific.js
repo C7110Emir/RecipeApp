625c1b5d218fb966f7c503acbe1a7036fa5134f5
EmirBirlik <ninjakoal2002@gmail.com>
2021-04-21 10:27:20 -0400

    edit

---
+++ specific.js
@@ -0,0 +1,21 @@
+import data from "./api.js";
+if (sessionStorage.clickvalue == 1) {
+    if (sessionStorage.contentclick == 1) {
+        document.getElementById("carouselfirstimg").src = data["salads"][0].image1;
+        document.getElementById("carouselsecondimg").src = data["salads"][0].image2;
+        document.getElementById("carouselthirdimg").src = data["salads"][0].image3;
+        document.getElementById("descriptioncard").innerHTML = data["salads"][0].description;
+        document.getElementById("servingscard").innerHTML = data["salads"][0].Servings;
+        document.getElementById("ingredientsbody").innerHTML = data["salads"][0].ingredients;
+        document.getElementById("preperationbody").innerHTML = data["salads"][0].preparation;
+    }
+    if (sessionStorage.contentclick == 2) {
+        document.getElementById("carouselfirstimg").src = data["salads"][1].image1;
+        document.getElementById("carouselsecondimg").src = data["salads"][1].image2;
+        document.getElementById("carouselthirdimg").src = data["salads"][1].image3;
+        document.getElementById("descriptioncard").innerHTML = data["salads"][1].description;
+        document.getElementById("servingscard").innerHTML = data["salads"][1].Servings;
+        document.getElementById("ingredientsbody").innerHTML = data["salads"][1].ingredients;
+        document.getElementById("preperationbody").innerHTML = data["salads"][1].preparation;
+    }
+}
--- specific.ts
+++ specific.ts
@@ -1,1 +1,22 @@
 import data from "./api.js"
+
+if(sessionStorage.clickvalue == 1){
+    if(sessionStorage.contentclick == 1){
+        (document.getElementById("carouselfirstimg") as HTMLImageElement).src = data["salads"][0].image1;
+        (document.getElementById("carouselsecondimg") as HTMLImageElement).src = data["salads"][0].image2;
+        (document.getElementById("carouselthirdimg") as HTMLImageElement).src = data["salads"][0].image3;
+        (document.getElementById("descriptioncard") as HTMLElement).innerHTML = data["salads"][0].description;
+        (document.getElementById("servingscard") as HTMLElement).innerHTML = data["salads"][0].Servings;
+        (document.getElementById("ingredientsbody") as HTMLElement).innerHTML = data["salads"][0].ingredients;
+        (document.getElementById("preperationbody") as HTMLElement).innerHTML = data["salads"][0].preparation
+    }
+    if(sessionStorage.contentclick == 2){
+        (document.getElementById("carouselfirstimg") as HTMLImageElement).src = data["salads"][1].image1;
+        (document.getElementById("carouselsecondimg") as HTMLImageElement).src = data["salads"][1].image2;
+        (document.getElementById("carouselthirdimg") as HTMLImageElement).src = data["salads"][1].image3;
+        (document.getElementById("descriptioncard") as HTMLElement).innerHTML = data["salads"][1].description;
+        (document.getElementById("servingscard") as HTMLElement).innerHTML = data["salads"][1].Servings;
+        (document.getElementById("ingredientsbody") as HTMLElement).innerHTML = data["salads"][1].ingredients;
+        (document.getElementById("preperationbody") as HTMLElement).innerHTML = data["salads"][1].preparation
+    }
+}
