65bed962aad31046cf3f7df268064d82e2f529e4
EmirBirlik <ninjakoal2002@gmail.com>
2021-04-21 10:30:17 -0400

    edit

--- specific.js
+++ specific.js
@@ -18,4 +18,13 @@ if (sessionStorage.clickvalue == 1) {
         document.getElementById("ingredientsbody").innerHTML = data["salads"][1].ingredients;
         document.getElementById("preperationbody").innerHTML = data["salads"][1].preparation;
     }
+    if (sessionStorage.contentclick == 3) {
+        document.getElementById("carouselfirstimg").src = data["salads"][2].image1;
+        document.getElementById("carouselsecondimg").src = data["salads"][2].image2;
+        document.getElementById("carouselthirdimg").src = data["salads"][2].image3;
+        document.getElementById("descriptioncard").innerHTML = data["salads"][2].description;
+        document.getElementById("servingscard").innerHTML = data["salads"][2].Servings;
+        document.getElementById("ingredientsbody").innerHTML = data["salads"][2].ingredients;
+        document.getElementById("preperationbody").innerHTML = data["salads"][2].preparation;
+    }
 }
