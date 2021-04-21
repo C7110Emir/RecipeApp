d41a9172dd22b00ea6df39dad5d86d44970bd234
EmirBirlik <ninjakoal2002@gmail.com>
2021-04-21 07:43:36 -0400

    converted to triple ts

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var i;
+var saladbutton = document.getElementById("saladsbutton");
+var dessertbutton = document.getElementById("dessertsbutton");
+var soupbutton = document.getElementById("soupsbutton");
+saladbutton.addEventListener("click", buttonclick);
+dessertbutton.addEventListener("click", buttonclick);
+soupbutton.addEventListener("click", buttonclick);
+function buttonclick() {
+}
---
+++ main.ts
@@ -0,0 +1,16 @@
+import data from "./api.js"
+
+
+let i;
+
+const saladbutton = document.getElementById("saladsbutton")
+const dessertbutton = document.getElementById("dessertsbutton")
+const soupbutton = document.getElementById("soupsbutton")
+
+saladbutton.addEventListener("click",buttonclick)
+dessertbutton.addEventListener("click",buttonclick)
+soupbutton.addEventListener("click",buttonclick)
+
+function buttonclick(){
+
+}
