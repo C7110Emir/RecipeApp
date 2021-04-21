2d8e3370e81963f80e3adddfd0d7796fa3e16af6
EmirBirlik <ninjakoal2002@gmail.com>
2021-04-21 09:40:27 -0400

    edit

--- content.js
+++ content.js
@@ -17,20 +17,35 @@ if (sessionStorage.clickvalue == 1) {
     document.getElementById("thirdcarddescription").innerHTML = data["salads"][2].description;
     document.getElementById("thirdcardbottom").innerHTML = data["salads"][2].preptime;
 }
-
-else if(sessionStorage.clickvalue == 2){
+else if (sessionStorage.clickvalue == 2) {
     document.getElementById("firstcontentimage").src = data["desserts"][0].image1;
     document.getElementById("firstcontenttitle").innerHTML = data["desserts"][0].name;
     document.getElementById("firstcarddescription").innerHTML = data["desserts"][0].description;
     document.getElementById("firstcardbottom").innerHTML = data["desserts"][0].preptime;
-     //second content
-     document.getElementById("secondcontentimage").src = data["desserts"][1].image1;
-     document.getElementById("secondcontenttitle").innerHTML = data["desserts"][1].name;
-     document.getElementById("secondcarddescription").innerHTML = data["desserts"][1].description;
-     document.getElementById("secondcardbottom").innerHTML = data["desserts"][1].preptime;
-     //third content
-     document.getElementById("thirdcontentimage").src = data["desserts"][2].image1;
-     document.getElementById("thirdcontenttitle").innerHTML = data["desserts"][2].name;
-     document.getElementById("thirdcarddescription").innerHTML = data["desserts"][2].description;
-     document.getElementById("thirdcardbottom").innerHTML = data["desserts"][2].preptime;
+    //second content
+    document.getElementById("secondcontentimage").src = data["desserts"][1].image1;
+    document.getElementById("secondcontenttitle").innerHTML = data["desserts"][1].name;
+    document.getElementById("secondcarddescription").innerHTML = data["desserts"][1].description;
+    document.getElementById("secondcardbottom").innerHTML = data["desserts"][1].preptime;
+    //third content
+    document.getElementById("thirdcontentimage").src = data["desserts"][2].image1;
+    document.getElementById("thirdcontenttitle").innerHTML = data["desserts"][2].name;
+    document.getElementById("thirdcarddescription").innerHTML = data["desserts"][2].description;
+    document.getElementById("thirdcardbottom").innerHTML = data["desserts"][2].preptime;
+}
+else if (sessionStorage.clickvalue == 3) {
+    document.getElementById("firstcontentimage").src = data["soups"][0].image1;
+    document.getElementById("firstcontenttitle").innerHTML = data["soups"][0].name;
+    document.getElementById("firstcarddescription").innerHTML = data["soups"][0].description;
+    document.getElementById("firstcardbottom").innerHTML = data["soups"][0].preptime;
+    //second content
+    document.getElementById("secondcontentimage").src = data["soups"][1].image1;
+    document.getElementById("secondcontenttitle").innerHTML = data["soups"][1].name;
+    document.getElementById("secondcarddescription").innerHTML = data["soups"][1].description;
+    document.getElementById("secondcardbottom").innerHTML = data["soups"][1].preptime;
+    //third content
+    document.getElementById("thirdcontentimage").src = data["soups"][2].image1;
+    document.getElementById("thirdcontenttitle").innerHTML = data["soups"][2].name;
+    document.getElementById("thirdcarddescription").innerHTML = data["soups"][2].description;
+    document.getElementById("thirdcardbottom").innerHTML = data["soups"][2].preptime;
 }
